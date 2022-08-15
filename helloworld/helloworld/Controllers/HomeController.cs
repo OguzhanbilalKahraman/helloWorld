ffa2aaf1a0e583fcb2b8aa193a2104e56a467091
Oğuzhan Bilal Kahraman <73184456+OguzhanbilalKahraman@users.noreply.github.com>
2022-08-15 14:00:32 +0300

    Add files via upload

---
+++ helloworld/helloworld/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloworld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Uygulama Calisiyor....";
+        }
+        public string Test()
+        {
+            return "bu test aksiyonudur.";
+        }
+
+        public IActionResult Selamla()
+        {
+            ViewResult result = View();
+
+             return result;
+           // return View();
+        }
+
+        public IActionResult Selamla2()
+        {
+            ViewResult result = View("Test2");
+
+            return result;
+            // return View();
+        }
+
+
+    }
+}
---
+++ helloworld/helloworld/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using helloworld.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloworld.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult GetUser()
+        {
+            User user = new User();
+            user.ID = 14;
+            user.FirstName = "oguz";
+            user.LastName = "kahraman";
+
+            return View();
+        }
+
+        [NonAction]// asagisini aksiyon almiyor.
+
+        public string GetFullName(User user)
+        {
+            return user.FirstName+" "+user.LastName;
+        }
+    }
+}
